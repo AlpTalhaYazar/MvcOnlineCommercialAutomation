ae06a7c17dba4798ec2eff3d744790891edfd8e1
Talha Yazar <talhayazarg@gmail.com>
2021-08-20 17:45:05 +0300

    Episode 13 : Gallery tab

---
+++ MvcOnlineCommercialAutomation/Controllers/GalleryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineCommercialAutomation.Models.Classes;
+
+namespace MvcOnlineCommercialAutomation.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+        Context con = new Context();
+
+        public ActionResult Index()
+        {
+            var vals1 = con.Products.ToList();
+            return View(vals1);
+        }
+    }
+}
