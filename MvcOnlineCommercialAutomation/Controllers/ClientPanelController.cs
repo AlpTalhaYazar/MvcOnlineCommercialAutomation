3ee696fa7f7e299cac059c2730e186cb52908de5
Talha Yazar <talhayazarg@gmail.com>
2021-09-01 23:28:42 +0300

    Episode 19 finished with full Client Login auth

--- MvcOnlineCommercialAutomation/Controllers/ClientPanelController.cs
+++ MvcOnlineCommercialAutomation/Controllers/ClientPanelController.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcOnlineCommercialAutomation.Models.Classes;
 
 namespace MvcOnlineCommercialAutomation.Controllers
 {
     public class ClientPanelController : Controller
     {
         // GET: ClientPanel
+        Context con = new Context();
+
         [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var Mail = (string)Session["ClientMail"];
+            var vals1 = con.Clients.FirstOrDefault(x => x.ClientMail == Mail);
+            ViewBag.m = Mail;
+            return View(vals1);
+        }
+
+        public ActionResult MyOrders()
+        {
+            var Mail = (string)Session["ClientMail"];
+            var id = con.Clients.Where(x => x.ClientMail == Mail.ToString()).Select(y => y.ClientID).FirstOrDefault();
+            var vals2 = con.SalesTransactions.Where(x => x.ClientID == id).ToList();
+
+            return View(vals2);
         }
     }
 }
--- MvcOnlineCommercialAutomation/Controllers/LoginController.cs
+++ MvcOnlineCommercialAutomation/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using MvcOnlineCommercialAutomation.Models.Classes;
 
 namespace MvcOnlineCommercialAutomation.Controllers
@@ -22,11 +23,32 @@ namespace MvcOnlineCommercialAutomation.Controllers
             return PartialView();
         }
         [HttpPost]
-        public ActionResult Partial1(Client c)
+        public ActionResult Partial1(Client cR)
         {
-            con.Clients.Add(c);
+            con.Clients.Add(cR);
             con.SaveChanges();
             return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult ClientLogin1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ClientLogin1(Client cL)
+        {
+            var credential = con.Clients.FirstOrDefault(x => x.ClientMail == cL.ClientMail && x.ClientPassword == cL.ClientPassword);
+            if (credential != null)
+            {
+                FormsAuthentication.SetAuthCookie(credential.ClientMail, false);
+                Session["ClientMail"] = credential.ClientMail.ToString();
+                return RedirectToAction("Index", "ClientPanel");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
         }
     }
 }
